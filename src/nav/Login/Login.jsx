63e6ba6a60cc703a25db84fb900359cd8f6f20fb
Mariobuddy <rohitbuatt9755@gmail.com>
2023-12-04 10:07:23 +0530

    last and final commit 15

--- src/nav/Login/Login.jsx
+++ src/nav/Login/Login.jsx
@@ -10,6 +10,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Loading from "../../components/Loading/Loading";
 import { getToken } from "../../redux/features/auth";
 import { useDispatch } from "react-redux";
+import Cookies from "js-cookie";
 const Login = () => {
   const nav = useNavigate();
   let dispatch=useDispatch();
@@ -84,6 +85,7 @@ const Login = () => {
         const data = await res.json();
         if (res.status === 200) {
           dispatch(getToken());
+          console.log(Cookies.get("jwt"));
           nav("/");
           setFormData({
             password: "",
